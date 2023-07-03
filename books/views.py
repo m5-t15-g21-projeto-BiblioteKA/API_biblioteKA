6d16dafcb9a7ec40c8b43a51dd22f97ee7bc6b2e
nagilosantos <nagilosantos@gmail.com>
2023-07-03 17:43:07 -0300

    Feat/ create book view and url

--- books/views.py
+++ books/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Book
+from .seriliazers import BookSerializer
 
-# Create your views here.
+
+class BookView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookViewDetail(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
