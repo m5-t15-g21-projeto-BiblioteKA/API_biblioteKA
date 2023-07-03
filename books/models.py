10c2f716900ead5941243ea2f428ddfb8e57105e
Adryel César <adryelcesarm@gmail.com>
2023-07-03 11:34:59 -0300

    feat: cria models

--- books/models.py
+++ books/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    author = models.CharField(max_length=50)
+    book_genre = models.CharField(max_length=50)
+    synopsis = models.CharField(max_length=200)
+    publishing_company = models.CharField(max_length=50)
+    num_copies = models.IntegerField()
+    
+    users = models.ManyToManyField('users.User', related_name='books' )
+
--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    password = models.CharField(max_length=127)
+    is_colaborator = models.BooleanField(null=True, default=False)
+    can_locate = models.BooleanField(default=True)
+    
+    user_rents = models.ManyToManyField('copies.Copy', through='rents.Rent', related_name='users_rents')
