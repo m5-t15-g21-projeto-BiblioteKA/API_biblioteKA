490c509c5ce84555280bc7755dfd422b1b5b0b93
Adryel César <adryelcesarm@gmail.com>
2023-07-04 09:14:43 -0300

    feat rotas user

--- users/views.py
+++ users/views.py
@@ -1,9 +1,18 @@
-from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView, ListCreateAPIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from .models import User
 from .serializers import UserSerializer
+from rest_framework import generics
+from .permissions import IsAccountOwner
 
 
-class UserView(CreateAPIView):
+class UserView(ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAccountOwner]
+    
     queryset = User.objects.all()
     serializer_class = UserSerializer
