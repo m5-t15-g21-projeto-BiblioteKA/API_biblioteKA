4541e2946ae3215b366bb8b6421004c63f9d0b11
J. Fernandes <j.fernandes8077@gmail.com>
2023-07-04 17:51:56 -0300

    fix: change books serializer

--- books/seriliazers.py
+++ books/seriliazers.py
@@ -1,11 +1,20 @@
 from rest_framework import serializers
 from .models import Book
 
+from copies.models import Copy
+
 
 class BookSerializer(serializers.ModelSerializer):
-
     def create(self, validated_data: dict) -> Book:
-        return Book.objects.create(**validated_data)
+        book = Book.objects.create(**validated_data)
+
+        number_copies = book.num_copies
+
+        while number_copies > 0:
+            Copy.objects.create(book=book)
+            number_copies -= 1
+
+        return book
 
     class Meta:
         model = Book
@@ -16,5 +25,5 @@ class BookSerializer(serializers.ModelSerializer):
             "book_genre",
             "synopsis",
             "publishing_company",
-            "num_copies"
+            "num_copies",
         ]
