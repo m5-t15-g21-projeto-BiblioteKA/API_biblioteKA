f47beb34eb910e5c2c804e2125cb2ee07aec33d5
nagilosantos <nagilosantos@gmail.com>
2023-07-04 10:01:57 -0300

    feat/ create book permission and fix on views

--- books/views.py
+++ books/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Book
 from .seriliazers import BookSerializer
-from ..users.permissions import IsAccountOwner
+from .permissions import IsSuperuserOrReadOnly
 
 
 class BookView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAccountOwner]
+    permission_classes = [IsSuperuserOrReadOnly]
 
 
 class BookViewDetail(generics.RetrieveAPIView):
