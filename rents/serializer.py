e1a3551b2da523133dff14679ec3223a4bc9ae9c
nagilosantos <nagilosantos@gmail.com>
2023-07-05 20:15:04 -0300

    Fix/ create a rent is now working

--- rents/serializer.py
+++ rents/serializer.py
@@ -5,7 +5,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from .models import Rent
 from users.serializers import UserSerializer
 from copies.serializers import CopySerialzer
-
+from books.seriliazers import BookSerializer
 from copies.models import Copy
 from users.models import User
 from datetime import datetime, timedelta
@@ -13,15 +13,18 @@ from datetime import datetime, timedelta
 
 class RentSerializer(serializers.ModelSerializer):
     book = serializers.IntegerField(write_only=True)
+    user_id = serializers.IntegerField(write_only=True)
+    user = UserSerializer(read_only=True)
+    book_details = BookSerializer(source='copy.book', read_only=True)
 
     def create(self, validated_data):
         book_id = validated_data.pop("book", None)
+        user_id = validated_data.pop("user_id", None)
 
-        print("\n")
-        print("\n")
-        print(validated_data)
-        print("\n")
-        print("\n")
+        try:
+            user = User.objects.get(id=user_id)
+        except User.DoesNotExist:
+            raise ValidationError({"msg": "Invalid user ID"})
 
         copy = Copy.objects.filter(book_id=book_id, available=True).first()
 
@@ -40,18 +43,12 @@ class RentSerializer(serializers.ModelSerializer):
             date_limit += timedelta(days=days_to_add)
 
         validated_data["date_limit"] = date_limit
-
-        print("\n")
-        print("\n")
-        print(validated_data)
-        print("\n")
-        print("\n")
+        validated_data["user"] = user
+        validated_data["user_id"] = user.id
 
         return Rent.objects.create(**validated_data)
 
-    # user = UserSerializer(read_only=True)
-    # copy = CopySerialzer(read_only=True)
-
     class Meta:
         model = Rent
-        fields = ["id", "date_rent", "date_limit", "date_devolution", "user", "book"]
+        fields = ["id", "date_rent", "date_limit",
+                  "date_devolution", "user", "user_id", "book", "book_details"]
