b56f7db985597ae01e8eac88c59964f9bceecd86
Vitor Hugo Mendes <vitor_hugorj@hotmail.com>
2023-07-11 12:25:41 -0300

    fix: userPermissions

--- users/permissions.py
+++ users/permissions.py
@@ -3,21 +3,10 @@ from .models import User
 from rest_framework.views import Request, View
 
 
-class IsAccountOwnerOrReadOnly(permissions.BasePermission):
+class IsAdminOrCreateOnly(permissions.BasePermission):
     def has_permission(self, request: Request, view: View) -> bool:
         if request.method in permissions.SAFE_METHODS:
-            return True
-        user = User.objects.get(pk=view.kwargs["pk"])
-        return bool(request.user == user)
-
-
-class CreateOnly(permissions.BasePermission):
-    def has_permission(self, request: Request, view: View) -> bool:
-        if request.method in permissions.SAFE_METHODS:
-            if request.user.is_authenticated:
-                return bool(request.user.is_colaborator)
-            else:
-                return False
+            return bool(request.user.is_colaborator)
         return True
 
 
